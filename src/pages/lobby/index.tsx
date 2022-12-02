7521b83aedf2074c330e0a48f9813d29761e297d
NathanPip <107423247+NathanPip@users.noreply.github.com>
2022-12-01 17:21:14 -0800

    change auth to create lobby

--- src/pages/lobby/index.tsx
+++ src/pages/lobby/index.tsx
@@ -1,12 +1,16 @@
 import { type NextPage } from "next";
 import { useRouter } from "next/router";
 import { useRef } from "react";
+import { useSession } from "next-auth/react";
+
 
 const Lobby: NextPage = () => {
 
     const router = useRouter();
     const input = useRef<HTMLInputElement>(null);
+    const { data: sessionData } = useSession();
 
+    if(!sessionData) return null;
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
