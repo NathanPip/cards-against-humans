39b19ea5e23d55c646f4657c0c9c28cc55c935ec
Christian Sanchez <52680827+bigolchris@users.noreply.github.com>
2022-12-23 18:31:44 -0800

    changes some styles in CAH options
    -removed hard borders
    -added inset box shadows
    -layout of card pack options changed

--- src/components/Lobby/LobbyScreen.tsx
+++ src/components/Lobby/LobbyScreen.tsx
@@ -18,15 +18,21 @@ const LobbyScreen: React.FC = () => {
 
   return (
     <>
-      <h1 className="flex justify-center text-2xl">{title}</h1>
+      <h1 className="mb-4 flex justify-center text-4xl">{title}</h1>
       {/* <p className="flex justify-end">{id}</p> */}
-      <div className="ml-9 h-48 w-48 rounded-2xl border-2 border-black bg-zinc-800/40">
-        <h2 className="mt-4 flex w-48 justify-center ">Current Party</h2>
-        {others.map((name) => (
-          <div className="flex justify-center text-2xl" key={name[0]}>
-            {name[1]}
+      <div className="max-w-full ">
+        <h2 className=" mt-8 mb-4 flex w-full  justify-center text-lg">
+          Players
+        </h2>
+        <div className="max-w-70  mb-4 h-44 shadow-inset ">
+          <div className=" ml-8 h-24 max-w-full  ">
+            {others.map((name) => (
+              <div className=" flex justify-start text-lg" key={name[0]}>
+                {name[1]}
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
       {isHost && <GameSelect name="Cards Against Humanity"></GameSelect>}
     </>
